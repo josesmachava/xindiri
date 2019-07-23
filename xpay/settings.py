3744decf4860eb921ccee364bfb78f622e284b83
jose machava <jose.s.machava@gmail.com>
2019-07-23 23:36:46 +0200

    finish pagementos

--- xpay/settings.py
+++ xpay/settings.py
@@ -136,4 +136,8 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'xpay', 'staitc'),
+)
+
 django_heroku.settings(locals())
