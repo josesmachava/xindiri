89d9ab59d10d911f03a6682d4e7b51a1e166597d
jeremias <you@example.com>
2019-07-23 22:52:25 +0200

    model  user

---
+++ account/models.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    phone_number = models.CharField(max_length=255, null=False)
+    username = models.CharField(max_length=255, null=False)
+    email  = models.TextField()
+    website = models.URLField()
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return "{} - {}  -  {}".format(self.phone_number, self.username, self.email)
