e27685e483cfe4afba4b7126aa0ef17f424bfcda
jeremias <you@example.com>
2019-07-24 17:25:29 +0200

    menu personilized

--- xpay/urls.py
+++ xpay/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('mpesa.urls')),
     url(r'dashboard', include('account.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
