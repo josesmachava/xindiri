04f83bb8409524485b4c173fe80caf3e154713eb
jeremias <you@example.com>
2019-07-24 23:41:49 +0200

    table filled

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+
 # from account.models import User
+from mpesa.models import payment
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 @login_required()
 def index(request):
-    return render(request, 'dashboard/index.html')
+    paymentByUser = payment.objects.all()
+    return render(request, 'dashboard/index.html',{'payments': paymentByUser})
