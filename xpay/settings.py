42c1213c4c918577b7873c76f9f8f828e1143ce2
jose machava <jose.s.machava@gmail.com>
2019-07-24 16:06:18 +0200

    implement header

--- xpay/settings.py
+++ xpay/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'mpesa',
     'account',
+    'xpay',
 ]
 
 MIDDLEWARE = [
@@ -134,10 +135,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'xpay', 'staitc'),
+    os.path.join(BASE_DIR, 'xpay', 'static'),
 )
 
 django_heroku.settings(locals())
