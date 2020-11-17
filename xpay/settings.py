2072d8617df2b5dc01e0b4863156d62973bc16fe
= <jose.s.machava@gmail.com>
2020-11-17 14:11:00 +0200

    put high

--- xpay/settings.py
+++ xpay/settings.py
@@ -67,13 +67,7 @@ GRAPHENE = {
 
 
 RQ_QUEUES = {
-    'default': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
-        'PASSWORD': 'some-password',
-        'DEFAULT_TIMEOUT': 360,
-    },
+
     'with-sentinel': {
         'SENTINELS': [('localhost', 26736), ('localhost', 26737)],
         'MASTER_NAME': 'redismaster',
@@ -88,11 +82,7 @@ RQ_QUEUES = {
         'URL': os.getenv('REDISTOGO_URL', 'redis://localhost:6379/0'), # If you're on Heroku
         'DEFAULT_TIMEOUT': 500,
     },
-    'low': {
-        'HOST': 'localhost',
-        'PORT': 6379,
-        'DB': 0,
-    }
+
 }
 
 
