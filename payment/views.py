571103e351af8d07a95badf6d8a549632a78e1f6
= <jose.s.machava@gmail.com>
2020-11-18 03:10:45 +0200

    improve request

--- payment/views.py
+++ payment/views.py
@@ -14,7 +14,7 @@ from .models import Package, Order
 
 
 @login_required()
-@job('high')
+
 def mpesa(request, pk):
     package = Package.objects.get(pk=pk)
     if request.method == "POST":
@@ -27,16 +27,17 @@ def mpesa(request, pk):
             payment.user = request.user
             payment.order = order
 
-            API_ENDPOINT = "https://development-xindiri.herokuapp.com/v1/payments/"
+            API_ENDPOINT = "https://xpayy.herokuapp.com/payment/"
             data = {
 
-                'phone_number': payment.número_de_telefone,
-                'amount': package.price,
-                'api_key': 'a0a9fe0bf9178657835ab0ad4b033f9f',
+                'contact': payment.número_de_telefone,
+                'amount': "1",
+                'reference': secrets.token_hex(4),
+                'api_key': 'cpurgkttk6fw317ucnnxaqxcnrkxszgs',
+                'public_key': " MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAyrOP7fgXIJgJyp6nP/Vtlu8kW94Qu+gJjfMaTNOSd/mQJChqXiMWsZPH8uOoZGeR/9m7Y8vAU83D96usXUaKoDYiVmxoMBkfmw8DJAtHHt/8LWDdoAS/kpXyZJ5dt19Pv+rTApcjg7AoGczT+yIU7xp4Ku23EqQz70V5Rud+Qgerf6So28Pt3qZ9hxgUA6lgF7OjoYOIAKPqg07pHp2eOp4P6oQW8oXsS+cQkaPVo3nM1f+fctFGQtgLJ0y5VG61ZiWWWFMOjYFkBSbNOyJpQVcMKPcfdDRKq+9r5DFLtFGztPYIAovBm3a1Q6XYDkGYZWtnD8mDJxgEiHWCzog0wZqJtfNREnLf1g2ZOanTDcrEFzsnP2MQwIatV8M6q/fYrh5WejlNm4ujnKUVbnPMYH0wcbXQifSDhg2jcnRLHh9CF9iabkxAzjbYkaG1qa4zG+bCidLCRe0cEQvt0+/lQ40yESvpWF60omTy1dLSd10gl2//0v4IMjLMn9tgxhPp9c+C2Aw7x2Yjx3GquSYhU6IL41lrURwDuCQpg3F30QwIHgy1D8xIfQzno3XywiiUvoq4YfCkN9WiyKz0btD6ZX02RRK6DrXTFefeKjWf0RHREHlfwkhesZ4X168Lxe9iCWjP2d0xUB+lr10835ZUpYYIr4Gon9NTjkoOGwFyS5ECAwEAAQ==        ",
             }
             # sending post request and saving response as response object
             payment_data = requests.post(url=API_ENDPOINT, data=data)
-
 
             response = json.loads(payment_data.text)
             status_code = response['transaction_status_code']
@@ -65,8 +66,6 @@ def mpesa(request, pk):
 
 
 
-
-mpesa.delay()
 
 
 
